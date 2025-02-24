7e7ebb9075d713af5c16414f0ff794c98da35e96
Gato931 <125412779+Gato931@users.noreply.github.com>
2025-02-24 03:37:28 -0500

    Delete proyectofinal directory

--- proyectofinal/src/main/java/co/edu/uniquindio/poo/exceptions/ContraseñaIncorrectaExeption.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo.exceptions;
-
-public class ContraseñaIncorrectaExeption extends RuntimeException {
-  public ContraseñaIncorrectaExeption(String mensaje){
-    super("Su contraseña es incorrecta" + mensaje);
-  }
-}
-//throw new ContraseñaIncorrectaExeption(contraseña);
